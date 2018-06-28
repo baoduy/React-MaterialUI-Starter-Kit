b8ebda61991708bf7ae66e8458e1f5e2f3ca2e72
Steven Hoang <duy.hoang@schroders.com>
2018-06-28 17:43:15 +0800

    update the format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 4],
+    "linebreak-style": ["warn", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  }
 };
