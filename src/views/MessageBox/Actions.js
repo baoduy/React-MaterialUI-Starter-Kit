dfe47364c0e2bf3a24090b88fd533ec6b51a2429
Steven Hoang <drunkcoding@outlook.com>
2018-07-01 22:00:35 +0800

    add try catch to showMessage action

--- src/views/MessageBox/Actions.js
+++ src/views/MessageBox/Actions.js
@@ -1,48 +1,56 @@
 import * as actionTypes from "./ActionTypes";
 
 function createMessageId() {
-  return new Date().getTime();
+    return new Date().getTime();
 }
 export function showMessage(type, message, handler) {
-  return function(dispatch) {
-    const callback = event => {
-      if (handler) handler(event);
-      dispatch({ type: actionTypes.HIDE_MESSAGE_BOX });
-    };
+    return function (dispatch) {
+        const callback = event => {
+            try {
+                if (handler) handler(event);
+            } finally {
+                dispatch({
+                    type: actionTypes.HIDE_MESSAGE_BOX
+                });
+            }
+        };
 
-    dispatch({
-      type: actionTypes.SHOW_MESSAGE_BOX,
-      payload: {
-        id: createMessageId(),
-        type,
-        message,
-        open: true,
-        handler: callback
-      }
-    });
-  };
+        dispatch({
+            type: actionTypes.SHOW_MESSAGE_BOX,
+            payload: {
+                id: createMessageId(),
+                type,
+                message,
+                open: true,
+                handler: callback
+            }
+        });
+    };
 }
 
 export function notify(type, message) {
-  return function(dispatch) {
-    const id = createMessageId();
+    return function (dispatch) {
+        const id = createMessageId();
 
-    const callback = () => {
-      dispatch({
-        type: actionTypes.HIDE_NOTIFICATION,
-        payload: { id, message: "" }
-      });
+        const callback = () => {
+            dispatch({
+                type: actionTypes.HIDE_NOTIFICATION,
+                payload: {
+                    id,
+                    message: ""
+                }
+            });
+        };
+
+        dispatch({
+            type: actionTypes.SHOW_NOTIFICATION,
+            payload: {
+                id,
+                type,
+                message,
+                open: true,
+                closeNotification: callback
+            }
+        });
     };
-
-    dispatch({
-      type: actionTypes.SHOW_NOTIFICATION,
-      payload: {
-        id,
-        type,
-        message,
-        open: true,
-        closeNotification: callback
-      }
-    });
-  };
 }
