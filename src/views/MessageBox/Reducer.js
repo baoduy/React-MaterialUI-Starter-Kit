414a7c877258a3ca6bc6b91eaac3fa8869c9cbf3
Steven Hoang <drunkcoding@outlook.com>
2018-07-07 21:20:56 +0800

    using react-redux-thunk-store

--- src/views/MessageBox/Reducer.js
+++ src/views/MessageBox/Reducer.js
@@ -1,53 +1,52 @@
-import InitialState from "../../reducers/initialState";
 import * as actionTypes from "./ActionTypes";
 import linq from "linq";
 
-export function MessageBoxReducer(state = InitialState.messageBox, action) {
-    switch (action.type) {
-        case actionTypes.SHOW_MESSAGE_BOX:
-            return Object.assign(action.payload, {
-                open: true
-            });
-        case actionTypes.HIDE_MESSAGE_BOX:
-            return Object.assign({}, state, {
-                message: "",
-                handler: undefined,
-                open: false
-            });
-        default:
-            return state;
-    }
+export function MessageBoxReducer(state = {}, action) {
+  switch (action.type) {
+    case actionTypes.SHOW_MESSAGE_BOX:
+      return Object.assign(action.payload, {
+        open: true
+      });
+    case actionTypes.HIDE_MESSAGE_BOX:
+      return Object.assign({}, state, {
+        message: "",
+        handler: undefined,
+        open: false
+      });
+    default:
+      return state;
+  }
 }
 
 export function NotificationReducer(
-    state = InitialState.notifications,
-    action
+  state = [],
+  action
 ) {
-    switch (action.type) {
-        case actionTypes.SHOW_NOTIFICATION:
-            {
-                //filter out the closed notification and add new item on top
-                const items = linq
-                    .from(state)
-                    .where(i => i.open)
-                    .toArray();
-                return [Object.assign(action.payload, {
-                    open: true
-                }), ...items];
-            }
-        case actionTypes.HIDE_NOTIFICATION:
-            {
-                //set open to false
-                const items = linq
-                    .from(state)
-                    .where(i => i.id != action.payload.id)
-                    .toArray();
-                return [...items, Object.assign(action.payload, {
-                    message: "",
-                    open: false
-                })];
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case actionTypes.SHOW_NOTIFICATION:
+      {
+        //filter out the closed notification and add new item on top
+        const items = linq
+          .from(state)
+          .where(i => i.open)
+          .toArray();
+        return [Object.assign(action.payload, {
+          open: true
+        }), ...items];
+      }
+    case actionTypes.HIDE_NOTIFICATION:
+      {
+        //set open to false
+        const items = linq
+          .from(state)
+          .where(i => i.id != action.payload.id)
+          .toArray();
+        return [...items, Object.assign(action.payload, {
+          message: "",
+          open: false
+        })];
+      }
+    default:
+      return state;
+  }
 }
