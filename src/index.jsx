a30313e926e050112cafb30b2877e7d3f4df3657
Steven Hoang <drunkcoding@outlook.com>
2018-08-02 22:54:41 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,24 @@
 /*eslint no-console: ["off", { allow: ["warn", "error"] }] */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Router, Route, Switch, BrowserRouter } from 'react-router-dom';
 import Provider from 'react-redux-thunk-store';
 import ExceptionHandler from './layouts/ExceptionHandler';
-
 //Style-sheets
 import './assets/less/material-dashboard-react.less';
 
 import reducers from './reducers';
 import indexRoutes from 'routes/index.jsx';
 
+//indicate whether application is running on PRD or not
+const isPrd = process.env.NODE_ENV === 'production';
+
 //Update for Reserved proxy
 window._base = document.getElementsByTagName('base')[0].getAttribute('href');
 console.log(`base URL ${window._base}`);
 
 const hist = createBrowserHistory({ basename: window._base });
-//indicate whether application is running on PRD or not
-const isPrd = process.env.NODE_ENV === 'production';
 
 const createRouter = () => {
   return (
